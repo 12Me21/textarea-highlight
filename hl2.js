75ab203784b75a5ed2a71f1de11f46fb2c3d2a09
12Me21 <12Me21.mc@gmail.com>
2022-06-06 08:00:41 -0400

    improve parser def

--- hl2.js
+++ hl2.js
@@ -1,40 +1,66 @@
-class State extends RegExp {
-	constructor(parent, ...patterns) {
-		let groups = []
-		let r = patterns.join("|")
-			.replace(/\n/g, "|").replace(/\\`/g, "`")
-			.replace(/[(](?![?])/g, "(?:")
-			.replace(/[(][?]<(.*?)_(.*?)>[)]/g, (m, name, state)=>{
-				groups.push([name, state])
-				return "()"
-			})
-		super(r, 'g')
-		this.groups = groups
-		this.parent = parent
-	}
-	scan() {
-		this.lastIndex = this.parent.lastIndex
-		let match = this.exec(this.parent.text)
-		if (match) {
-			let before = this.parent.text.substring(this.parent.lastIndex, match.index)
-			this.parent.lastIndex = this.lastIndex
-			let num = match.indexOf("", 1)-1
-			return [before, this.groups[num], match[0]]
+function pre(text, cls) {
+	let p = document.createElement('span')
+	if (cls)
+		p.className = cls
+	p.append(text)
+	return p
+}
+
+class Parser {
+	constructor(states) {
+		this.lastIndex = 0
+		this.text = ""
+		this.states = Object.create(null)
+		for (let [name, patterns] of Object.entries(states)) {
+			let groups = []
+			let r = patterns.join("|")
+				.replace(/\n/g, "|").replace(/\\`/g, "`")
+				.replace(/[(](?![?])/g, "(?:")
+				.replace(/[(][?]<(.*?)_(.*?)>[)]/g, (m, name, state)=>{
+					groups.push([name, state])
+					return "()"
+				})
+			let regex = new RegExp(r, 'g')
+			this.states[name] = {regex, groups}
 		}
 	}
-}
-
-let parent = {lastIndex: 0, text: "<!doctype html> hii!~ <!--  hecko --> ... &lt;"}
-
-function make_parser(states) {
-	for (let [name, patterns] of states) {
-		
+	parse(text, out) {
+		out.textContent = ""
+		let current = 'data'
+		let prev = -1
+		let iloop = 0
+		let lastIndex = 0
+		while (1) {
+			if (lastIndex==prev) {
+				iloop++
+				if (iloop>5)
+					throw new Error('infinite loop '+lastIndex+"\n"+text.substring(lastIndex-10, 100))
+			} else
+				iloop=0
+			prev = lastIndex
+			let st = this.states[current]
+			st.regex.lastIndex = lastIndex
+			let match = st.regex.exec(text)
+			if (!match)
+				break
+			let before = text.substring(lastIndex, match.index)
+			lastIndex = st.regex.lastIndex
+			let num = match.indexOf("", 1)-1
+			let g = st.groups[num]
+			if (before)
+				out.append(pre(before))
+			out.append(pre(match[0], g[0]))
+			if (g[1])
+				current = g[1]
+		}
+		let after = text.substring(lastIndex)
+		if (after)
+			out.append(pre(after))
 	}
 }
 
-let states = {
-	data: new State(
-		parent,
+let htmlp = new Parser({
+	data: [
 		`&([a-zA-Z0-9]+|#[xX][0-9a-fA-F]+|#[0-9]+);?(?<charref_>)`,
 		`<![dD][oO][cC][tT][yY][pP][eE][^]*?(>|$)(?<doctype_>)`,
 		`<script>(?<tag_script>)`,
@@ -43,75 +69,29 @@ let states = {
 		`<!---?>(?<error_>)`,
 		`<!--(?<comment_comment>)`,
 		`<[!?/][^>]*(>|$)(?<error_>)`,
-	),
-	comment: new State(
-		parent,
+	],
+	comment: [
 		`[^]*?(?=--!?>|$)(?<commenttext_commentend>)`,
-	),
-	commentend: new State(
-		parent,
+	],
+	commentend: [
 		`--!?>(?<comment_data>)`,
-	),
-	script: new State(
-		parent,
+	],
+	script: [
 		`[^]*?(?=</script|$)(?<script_data>)`,
-	),
-	attributes: new State(
-		parent,
+	],
+	attributes: [
 		`[^\t\n\f />=]+(?<key_aftername>)`,
 		`>(?<tag_data>)`,
-	),
-	aftername: new State(
-		parent,
+	],
+	aftername: [
 		`\s*=\s*(?<_value>)`,
 		`(?<_attributes>)`,
-	),
-	value: new State(
-		parent,
+	],
+	value: [
 		`[^\t\n\f >]*(?<value_attributes>)`,
 		`(?<_attributes>)`,
-	),
-	rawtext: new State(
-		parent,
+	],
+	rawtext: [
 		`[^]*?(?=</style|$)(?<rawtext_data>)`,
-	)
-}
-/*$in.oninput = e=>{
-	go($in.value)
-}*/
-
-function pre(text, cls) {
-	let p = document.createElement('span')
-	if (cls)
-		p.className = cls
-	p.append(text)
-	return p
-}
-function go(text, $out) {
-	$out.textContent = ""
-	let current = 'data'
-	let prev = -1
-	let iloop = 0
-	parent.text = text
-	parent.lastIndex = 0
-	while (1) {
-		if (parent.lastIndex==prev) {
-			iloop++
-			if (iloop>5)
-				throw new Error('infinite loop '+parent.lastIndex)
-		} else
-			iloop=0
-		prev = parent.lastIndex
-		let res = states[current].scan()
-		if (!res)
-			break
-		let last = res[0]
-		if (last)
-			$out.append(pre(last))
-		$out.append(pre(res[2], res[1][0]))
-		current = res[1][1] || current
-	}
-	let last = parent.text.substring(parent.lastIndex)
-	if (last)
-		$out.append(pre(last))
-}
+	],
+})
