943a3ab5daf21c33891a686610c2fcf91d20f0cc
12Me21 <12Me21.mc@gmail.com>
2022-06-18 15:49:17 -0400

    replace invalid ctrl chars in overlay

--- hl-live.js
+++ hl-live.js
@@ -247,7 +247,7 @@ function render(t, out) {
 			out.insertBefore(elem1, elem2)
 			changed = true
 		}
-		let text = t.substr(ind, tokens[i].len)
+		let text = t.substr(ind, tokens[i].len).replace(/[\0-\10\13\14\16-\37\177]/g, "￿") // 
 		if (elem1.textContent != text) {
 			elem1.textContent = text
 			changed = true
@@ -268,3 +268,43 @@ function render(t, out) {
 	}
 	return pp
 }
+
+
+htmlp = new Parser({
+	data: STATE`
+(break|catch|class|continue|default|do|else|finally|for|function|if|switch|try|while|with|case|return|throw|yield|yield|=>)(?![\w$])${{token:'flow'}}
+(typeof|await|delete|void|in|instanceof|new)(?![\w$])${{token:'operator'}}
+[?]?[.]\s*${{state:'property'}}
+([+-]{2}|[!=]==?|[!~])${{token:'operator'}}
+=${{token:'assignment'}}
+([-*%+&^|]|[*<>&|?]{2}|>>>)(=${{token:'assignment'}})?${{token:'operator'}}
+([?]|:|[<>]=?)${{token:'operator'}}
+(super|this)(?![\w$])${{token:'keyword', state:'after_value'}}
+(const|debugger|export|import|var|enum|implements|interface|let|package|private|protected|public|static|extends)(?![\w$])${{token:'keyword'}}
+(?!\d)[\w$]+(?=\s*:)${{token:'property', state:'after_label'}}
+(?!\d)[\w$]+${{token:'word', state:'after_value'}}
+/(?![*/])([^/\n\\]+|\\.)*?(/[idgmuy]*|\n|$)${{token:'string', state:'after_value'}}
+"([^\n\\"]+|\\[^])*?("|\n|$)${{token:'string', state:'after_value'}}
+'([^\n\\']+|\\[^])*?('|\n|$)${{token:'string', state:'after_value'}}
+\`([^\\\`]+|\\[^])*?(\`|$)${{token:'string', state:'after_value'}}
+//.*${{token:'comment'}}
+/[*][^]*?([*]/|$)${{token:'comment'}}
+<!--.*${{token:'comment'}}
+(0[xXbBoO]|[.])?[\dA-Fa-f]+(_?[\dA-Fa-f]+)*${{token:'constant', state:'after_value'}}
+\n${{}}
+;${{token:'semicolon'}}
+`,
+	after_value: STATE`
+/=${{token:'assignment'}}
+/${{token:'operator'}}
+[+-]{2}${{token:'operator'}}
+${{state:'data'}}
+`,
+	property: STATE`
+(?!\d)[\w$]+${{token:'property', state:'data'}}
+${{state:'data'}}
+`,
+	after_label: STATE`
+\s*:${{state:'data'}}
+`
+})
