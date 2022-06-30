7891f2c6cc2e6ccb00029690b401f66e19d34c51
12Me21 <12Me21.mc@gmail.com>
2022-06-30 19:18:55 -0400

    graph

--- textarea-indent.js
+++ textarea-indent.js
@@ -8,28 +8,28 @@ function get_last_indent(str, end) {
 function capture_tab(input, state) {
 	input.dataset.capture_tab = state ? "yes" : ""
 }
-function setup_textarea(input, cb) {
-	Object.assign(input, {
-		onclick(e) { capture_tab(e.target, true) },
-		onfocus(e) { capture_tab(e.target, false) },
-		onblur(e) { capture_tab(e.target, false) },
-		onkeydown(e) {
-			if ('Tab'==e.key && !e.shiftKey && e.target.dataset.capture_tab) {
-				cb && cb()
+let events = {
+	onclick(e) { capture_tab(e.target, true) },
+	onfocus(e) { capture_tab(e.target, false) },
+	onblur(e) { capture_tab(e.target, false) },
+	onkeydown(e) {
+		if ('Tab'==e.key && !e.shiftKey && e.target.dataset.capture_tab) {
+			e.preventDefault()
+			document.execCommand('insertText', false, "\t")
+		}
+	},
+	onbeforeinput(e) {
+		capture_tab(e.target, true)
+		if ('insertLineBreak'==e.inputType) {
+			let indent = get_last_indent(e.target.value, e.target.selectionStart)
+			if (indent) {
 				e.preventDefault()
-				document.execCommand('insertText', false, "\t")
-			}
-		},
-		onbeforeinput(e) {
-			cb && cb()
-			capture_tab(e.target, true)
-			if ('insertLineBreak'==e.inputType) {
-				let indent = get_last_indent(e.target.value, e.target.selectionStart)
-				if (indent) {
-					e.preventDefault()
-					document.execCommand('insertText', false, "\n"+indent)
-				}
+				document.execCommand('insertText', false, "\n"+indent)
 			}
 		}
-	})
+	},
+}
+
+function setup_textarea(input) {
+	Object.assign(input, events)
 }
